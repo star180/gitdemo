fb5c6f809af7b341776b03a816a2f11f6f587e2b
weixing <1808857902@qq.com>
2019-05-09 16:32:36 +0800

    添加

---
+++ gitdemo/src/net/beeway/emer/TestDemo.java
@@ -0,0 +1,19 @@
+package net.beeway.emer;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+    }
+}
